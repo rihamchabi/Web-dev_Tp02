2931b412c86e956a7bef34e85bed1a06ce477fd0
rihamchabi <rmimi4225@gmail.com>
2026-04-16 14:25:25 -0400

    Update Script.js

--- Step4/Script.js
+++ Step4/Script.js
@@ -17,23 +17,12 @@ $("#gpaForm").submit(function (e) {
 
         success: function (res) {
 
-            let color = "alert-info";
-
-            if (res.gpa >= 3.7) color = "alert-success";
-            else if (res.gpa >= 3.0) color = "alert-primary";
-            else if (res.gpa >= 2.0) color = "alert-warning";
-            else color = "alert-danger";
-
             $("#result").html(
-                <div class="alert ${color}">
+                <div class="alert alert-info">
+                GPA: ${res.gpa.toFixed(2)} <br>
                 ${res.message}
                 </div>
             );
-
-            let percent = (res.gpa / 4) * 100;
-
-            $("#bar").css("width", percent + "%");
-            $("#bar").text(res.gpa.toFixed(2));
         }
     });
 });
